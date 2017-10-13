b586781b2660d8a3d2071638b67d373bb3929e7e
Terry-Roberson <rooberson.t782954@stu.sanjac.edu>
2017-10-13 01:01:54 -0500

    UARG-97 basic layout of variable class, updated doc blocks

--- src/JSON/Parser/variables.java
+++ src/JSON/Parser/variables.java
@@ -19,55 +19,93 @@
 * @since 1.0
 */
 package JSON.Parser;
-public class variables {
+public class variables<N, I, V, D> {
 	/**
 	 * Declares the details of the variables and assign them to generic types.
 	 *
-	 * @param <N> name of id, if given.
-	 * @param <I> ID of value.
-	 * @param <V> value of variable.
-	 * @param <D> description of variable.
+	 * @since 1.0
+	 */	
+	private N name;
+	private I id;
+	private V value;
+	private D description;
+	
+	/**
+	 * constructor
+	 *   
+	 * @param name  the names of the variables called
+	 * 
+	 * @param id  the ID of the called variable
+	 * 
+	 * @param value  the value of the called variable
+	 * 
+	 * @param description  the description of the called variable 
+	 * 
+	 * @return	<code> identifies value <code>
+	 * 			class returns specified value
 	 * @since 1.0
 	 */
-	public class variableInformation<N, I, V, D>{
-		
-		private N name;
-		private I id;
-		private V value;
-		private D description;
-		
-		/**
-		 * constructor
-		 *   
-		 * @param name = N
-		 * @param id = I
-		 * @param value = V
-		 * @param description = D
-		 * @since 1.0
-		 */
 		public void Seperation(N name, I id, V value, D description) {
 			this.name = name;
 			this.id = id;
 			this.value = value;
 			this.description = description;
 		}
-		
+		/**
+		 * getName method
+		 * 
+		 * @param N	the generic type associated with varName
+		 * 
+		 * @return name
+		 * 
+		 * @since 1.0
+		 */
 		private N getName(String){
 			return name;
 		}
+		
+		/**
+		 * getId method 
+		 * 
+		 * @param I	the generic type associated with varID
+		 * 
+		 * @return id
+		 * 
+		 * @since 1.0
+		 */
 		private I getId(){
 			return id;
 		}
+		
+		/**
+		 * getValue method
+		 * 
+		 *@param V	the generic type associated with varValue
+		 *
+		 *@return	value
+		 *
+		 * @since 1.0
+		 */
 		private V getValue(double){
 			private bool isVarIndependant;
 			
 		}
+		
+		/**
+		 * getDescription
+		 * 
+		 * @param D	the generic type associated with varDescription
+		 * 
+		 * @return description 
+		 * 
+		 * @since 1.0
+		 */
 		private D getDescription(String){
 			return description;
 		}
 		
-	}
+	
 	
 	
 
-}
+
