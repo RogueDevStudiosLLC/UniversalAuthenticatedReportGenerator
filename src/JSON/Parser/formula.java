0868b2b2786af21e42b9d8627bba7442bb6b89b0
Chel Hunter <Hunter.C845010@stu.sanjac.edu>
2017-10-23 21:00:06 -0500

    UARG-118 Messing with some stuff in the formula class, comments, docblocks

---
+++ src/JSON/Parser/formula.java
@@ -0,0 +1,48 @@
+/************************************************
+ * Formula Class File							*
+ * File name: formula.java						*
+ * The class file for a formula.				*
+ ***********************************************/
+ 
+package JSON.Parser;
+
+/**
+ * This class is a formula class.
+ * @author Chel
+ * @since 1.0
+ */
+public class Formula {
+/*Class Attributes*/
+	
+	/* The name meta of a formula*/
+	private String _formulaName;
+	/*The description of a formula*/
+	private String _formulaDesc;
+	/*The ID of a formula*/
+	private String _formulaId;
+	/*The base equation this formula uses*/
+	private String _formulaEquation;
+	
+/*Class Methods*/
+	
+/**
+ * 	Converts variable object values to
+ *  variable names compatible with exp4j.
+ *  
+ * @param Variable	Some variable objects
+ * 					to be used in/for the
+ * 					expression.
+ * 
+ */
+	private void _exp4jCompatibility (int Variable...) {
+		try {
+			// Setting variable values to exp4j compatible (temp?) variable names
+		} 
+		catch (invalidValueException e) {
+			// Exit this method/object, cancel export, go back to user input stage
+		}
+		// Exp4j here or in another method?
+	}
+	
+}
+
