6a8019b673932a0315e7d81bffbfe737ca27303f
Chel Hunter <Hunter.C845010@stu.sanjac.edu>
2017-10-23 21:39:01 -0500

    UARG 118 Reformatting, recommenting

--- src/JSON/Parser/formula.java
+++ src/JSON/Parser/formula.java
@@ -1,29 +1,31 @@
 /************************************************
  * Formula Class File							*
- * File name: formula.java						*
+ * File name: Formula.java						*
  * The class file for a formula.				*
  ***********************************************/
- 
+
 package JSON.Parser;
 
 /**
  * This class is a formula class.
+ * 
  * @author Chel
  * @since 1.0
  */
 public class Formula {
-/*Class Attributes*/
-	
-	/* The name meta of a formula*/
+	/* Class Attributes */
+
+	/* The name meta of a formula */
 	private String _formulaName;
-	/*The description of a formula*/
+	/* The description of a formula */
 	private String _formulaDesc;
-	/*The ID of a formula*/
+	/* The ID of a formula */
 	private String _formulaId;
-	/*The base equation this formula uses*/
+	/* The base equation this formula uses */
 	private String _formulaEquation;
-	
-/*Class Methods*/
+
+	/* Class Methods */
+	public void 
 	
 /**
  * 	Converts variable object values to
@@ -34,15 +36,14 @@ public class Formula {
  * 					expression.
  * 
  */
-	private void _exp4jCompatibility (int Variable...) {
+	private void _exp4jCompatibility (int Variable...) { // Error: Temp ellipses here until fleshed out
 		try {
 			// Setting variable values to exp4j compatible (temp?) variable names
 		} 
 		catch (invalidValueException e) {
 			// Exit this method/object, cancel export, go back to user input stage
 		}
-		// Exp4j here or in another method?
+		// Exp4j here or in another method? If here needs to not be void
 	}
 	
 }
-
