d288eb33fbf1dfd5f815d10fe6d8f509e5a358ff
Chel Hunter <Hunter.C845010@stu.sanjac.edu>
2017-10-24 17:35:27 -0500

    UARG-117 #time 20m Basic layout of some formula set class, if using, not a big issue currently, will have actual work and such later on once things are more fleshed

---
+++ src/JSON/Parser/FormulaSet.java
@@ -0,0 +1,50 @@
+/************************************************
+ * Formula Set Class File                       *
+ * File name: FormulaSet.java                   *
+ * The class file for a formula set.            *
+ ***********************************************/
+
+package JSON.Parser;
+
+/**
+ * This class is the formula set class.
+ * @author Chel
+ * @since 1.0
+ */
+public class FormulaSet {
+	/*Class Attributes*/
+	
+	/*Name of the formula set*/
+	private String _formulaSetName
+	/*Description of the formula set*/
+	private String _formulaSetDescription
+	
+	/*Class Methods */
+	
+	/**
+	 * Constructor for FormulaSet class. Sets values for the set name and set description.
+	 * @param setName	The name of the formula set.
+	 * @param setDescription	The formula set description.
+	 */
+	public FormulaSet(String setName, String setDescription) {
+		this._formulaSetName = setName;
+		this._formulaSetDescription = setDescription;
+		
+	}
+	
+	/**
+	 * Gets the formula set name.
+	 * @return The formula set name String.
+	 */
+	public String GetFormulaSetName() {
+		return this._formulaSetName;
+	}
+	
+	/**
+	 * Gets the formula set description.
+	 * @return The formula set description String.
+	 */
+	public String GetFormulaSetDesc() {
+		return this._formulaSetDescription;
+	}
+}
