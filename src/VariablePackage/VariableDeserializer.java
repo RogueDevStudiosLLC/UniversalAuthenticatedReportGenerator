a6b9f7dd18e84f7813cc7327043b45334bbca3e3
Terry-Roberson <roberson.t782954@stu.sanjac.edu>
2017-10-31 00:23:35 -0500

    UARG-123 more Variable Parser work

--- src/VariablePackage/VariableDeserializer.java
+++ src/VariablePackage/VariableDeserializer.java
@@ -1,14 +1,56 @@
+/********************************
+*  Variable Deserializer	    *
+*   File Name: 					*
+*   VariableDeserializer.java   *
+*                               *
+*   Variable Representation     *
+*   with meta data.		        *
+*                               *
+*  ©2017 Rogue Dev Studios, LLC *
+********************************/
 package VariablePackage;
 
-import com.google.gson.Gson;
+import java.lang.reflect.Type;
+import java.text.ParseException;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 
-
-public class VariableDeserializer {
+/**/
+public final class VariableDeserializer implements JsonDeserializer<Variable<?>>{
 	
-	private Gson gson;
 	
-	public VariableDeserializer() {
-		this.gson = new Gson();
+	public Variable<?> deserialize(final JsonElement json, final Type typeOf, final JsonDeserializationContext context)
+		throws JsonParseException{
+		
+		//deserialization code <3//
+		final JsonObject jsonObject = json.getAsJsonObject();
+		
+		final JsonElement jsonName = jsonObject.get("name");
+		final String _name = jsonName.getAsString();
+		
+		final String _id = jsonObject.get("ID").getAsString();
+		
+		final Type _valueType = new TypeToken<Variable<?>>() {}.getType();
+		
+		final String _description = jsonObject.get("description").getAsString();
+		
+		final boolean _requiresInput = jsonObject.get("requiresInput").getAsString() != null;
+		
+		final Variable<?> variable = new Variable();
+		
+		variable.SetName(_name);
+		variable.SetId(_id);
+		variable.SetValue(_value);
+		variable.SetDescription(_description);
+		variable.SetRequiresInput(_requiresInput);
+		return variable;
+		
 	}
+
+	
 }
+	
+	
+			
+
 
