2601fdaad4771d5e29d0cbd2805060f80ce631da
CHICHEN\Chichen <Rosales.G793036@stu.sanjac.edu>
2017-11-07 18:17:47 -0600

    Changes made to UI and stuff is on fire

---
+++ src/com/roguedevstudios/uarg/UI/MainController.java
@@ -0,0 +1,21 @@
+package com.roguedevstudios.uarg.UI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class MainController implements Initializable
+{
+	@FXML
+	
+	
+	public void initialize(URL location, ResourceBundle resources) 
+	
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
