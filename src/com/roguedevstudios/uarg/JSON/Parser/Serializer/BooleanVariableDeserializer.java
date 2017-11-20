350f503bf29c0da6d5967b1217700f8b66fb741c
Christopher Howard <otakuode@gmail.com>
2017-11-20 15:35:50 -0800

    SOP Compliance, Readability cleanup, Doc cleanup

--- src/com/roguedevstudios/uarg/JSON/Parser/Serializer/BooleanVariableDeserializer.java
+++ src/com/roguedevstudios/uarg/JSON/Parser/Serializer/BooleanVariableDeserializer.java
@@ -12,19 +12,21 @@ package com.roguedevstudios.uarg.JSON.Parser.Serializer;
 *****************************************/
 
 import java.lang.reflect.Type;
-
 import com.google.gson.*;
 import com.roguedevstudios.uarg.System.Core.Elements.Variable;
 
 /**
- * <p>
- * converts Variable.json information into Java Objects
- * <p>
+ * 
+ * Adapter class to Deserialize Variable objects of subtype Boolean
  * 
  * @author Terry Roberson
+ * @author Christopher E. Howard
  * @since 1.0
  */
-public class BooleanVariableDeserializer implements JsonDeserializer<Variable<Boolean>> {
+public class BooleanVariableDeserializer 
+	   implements JsonDeserializer<Variable<Boolean>> 
+{
+	
 	/**
 	 * Deserializes into a Variable<Boolean> type
 	 * from a given JsonElement, Type, and context
@@ -33,48 +35,58 @@ public class BooleanVariableDeserializer implements JsonDeserializer<Variable<Bo
 	 * @param JsonDeserializationContext
 	 * @return Variable<Boolean>
 	 */
-	
-	public Variable<Boolean> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-			throws JsonParseException {
+	public Variable<Boolean> deserialize( 
+										  JsonElement json, 
+										  Type typeOfT, 
+										  JsonDeserializationContext context
+										)
+										throws JsonParseException 
+	{
+		
 		/*Temp slot for variable name*/
 		String _name = null;
+		
 		/*Temp slot for variable ID*/
 		String _id = null;
+		
 		/*Temp slot for variable value*/
 		Boolean _value = null;
+		
 		/*Temp slot for variable description*/
 		String _description = null;
+		
 		/*Temp slot for variable requiresInput*/
 		boolean _requiresInput = false;
+		
 		/*Temp output object holder*/
 		Variable<Boolean> v;
 		
-		/* Convert JsonElement into JsonObject */
+		// Convert JsonElement into JsonObject
 		JsonObject o = json.getAsJsonObject();
 		
-		//If the object has a name, then we grab it
-		if(o.has("name")) {
+		// If the object has a name, then we grab it
+		if( o.has("name") )
 			_name = o.get("name").getAsString();
-		}
 		
-		//If the object has an ID, then we grab it
-		if(o.has("ID")) {
+		// If the object has an ID, then we grab it
+		if( o.has("ID") ) 
 			_id = o.get("ID").getAsString();
-		}
 		
-		//If the object has a value, then we grab it
-		if(o.has("value")) {
+		// If the object has a value, then we grab it
+		if( o.has("value") ) 
 			_value = o.get("value").getAsBoolean();
-		}
 		
-		//If the object has a description, then we grab it
-		if(o.has("description")) {
-			_description = o.get("description").getAsString();
-		}
+		// If the object has a description, then we grab it
+		if( o.has("description") ) 
+			_description = o.get("description").getAsString();		
 		
-		//Build the Variable object to return
-		v = new Variable<Boolean>(_name, _id, _requiresInput, _description, _value);
-			
+		// Build the Variable object to return
+		v = new Variable<Boolean>(  _name,
+									_id, 
+									_requiresInput, 
+									_description,
+									_value );
+		// Return the constructed variable object
 		return v;
 	}
 }
