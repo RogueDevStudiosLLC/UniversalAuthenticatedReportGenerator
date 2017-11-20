8a0ba12f0addb8393e699211a2012d196a3de7dd
Christopher Howard <otakuode@gmail.com>
2017-11-20 15:57:00 -0800

    SOP Compliance, Readability cleanup, Doc cleanup

--- src/com/roguedevstudios/uarg/JSON/Parser/Serializer/LongVariableDeserializer.java
+++ src/com/roguedevstudios/uarg/JSON/Parser/Serializer/LongVariableDeserializer.java
@@ -17,14 +17,16 @@ import com.google.gson.*;
 import com.roguedevstudios.uarg.System.Core.Elements.Variable;
 
 /**
- * <p>
- * converts Variable.json information into Java Objects
- * <p>
+ * 
+ * Adapter class to Deserialize Variable objects of subtype Long
  * 
  * @author Terry Roberson
+ * @author Christopher E. Howard
  * @since 1.0
  */
-public class LongVariableDeserializer implements JsonDeserializer<Variable<Long>> {
+public class LongVariableDeserializer 
+	   implements JsonDeserializer<Variable<Long>> 
+{
 	/**
 	 * Deserializes into a Variable<Long> type
 	 * from a given JsonElement, Type, and context
@@ -34,47 +36,59 @@ public class LongVariableDeserializer implements JsonDeserializer<Variable<Long>
 	 * @return Variable<Long>
 	 */
 	
-	public Variable<Long> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-			throws JsonParseException {
+	public Variable<Long> deserialize(
+										JsonElement json, 
+										Type typeOfT, 
+										JsonDeserializationContext context
+									 )
+									throws JsonParseException 
+	{
+		
 		/*Temp slot for variable name*/
 		String _name = null;
+		
 		/*Temp slot for variable ID*/
 		String _id = null;
+		
 		/*Temp slot for variable value*/
 		Long _value = null;
+		
 		/*Temp slot for variable description*/
 		String _description = null;
+		
 		/*Temp slot for variable requiresInput*/
 		boolean _requiresInput = false;
+		
 		/*Temp output object holder*/
 		Variable<Long> v;
 		
-		/* Convert JsonElement into JsonObject */
+		// Convert JsonElement into JsonObject
 		JsonObject o = json.getAsJsonObject();
 		
-		//If the object has a name, then we grab it
-		if(o.has("name")) {
+		// If the object has a name, then we grab it
+		if( o.has("name") ) 
 			_name = o.get("name").getAsString();
-		}
 		
-		//If the object has an ID, then we grab it
-		if(o.has("ID")) {
+		// If the object has an ID, then we grab it
+		if( o.has("ID") )
 			_id = o.get("ID").getAsString();
-		}
 		
-		//If the object has a value, then we grab it
-		if(o.has("value")) {
+		// If the object has a value, then we grab it
+		if( o.has("value") ) 
 			_value = o.get("value").getAsLong();
-		}
 		
-		//If the object has a description, then we grab it
-		if(o.has("description")) {
+		// If the object has a description, then we grab it
+		if( o.has("description") ) 
 			_description = o.get("description").getAsString();
-		}
 		
-		//Build the Variable object to return
-		v = new Variable<Long>(_name, _id, _requiresInput, _description, _value);
-			
+		// Build the Variable object to return
+		v = new Variable<Long>(	_name, 
+								_id, 
+								_requiresInput, 
+								_description, 
+								_value
+							  );
+		// Return the constructed variable object	
 		return v;
 	}
 }
