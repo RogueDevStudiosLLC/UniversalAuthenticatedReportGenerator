19839952a96fd92514b5083701aa7bbe31bbe946
Chel Hunter <Hunter.C845010@stu.sanjac.edu>
2017-11-28 18:44:15 -0600

    UARG-118 #time 10m fixed iformula stuff

--- src/com/roguedevstudios/uarg/System/Core/Elements/Interface/IFormula.java
+++ src/com/roguedevstudios/uarg/System/Core/Elements/Interface/IFormula.java
@@ -8,6 +8,7 @@ package com.roguedevstudios.uarg.System.Core.Elements.Interface;
 *                               *
 *  ©2017 Rogue Dev Studios, LLC *
 ********************************/
+import java.util.ArrayList;
 /**
  * Interface representing requirements for the Formula class
  * @author Chelsea Hunter
@@ -21,114 +22,66 @@ public interface IFormula {
 	 * Takes in an array of Number based Variables and
 	 * processes them through this formula returning an
 	 * Integer representation of the resulting answer.
-	 * @param args
+	 * @param vars	An array of Variable objects.
 	 * @return
 	 */
-	Integer CalculateToInteger(IVariable<?>[] args);
+	Integer CalculateToInteger(IVariable<?>[] vars);
 	/**
 	 * Takes in an array of Number based Variables and
 	 * processes them through this formula returning a
 	 * Doble representation of the resulting answer.
-	 * @param args
+	 * @param vars	An array of Variable objects.
 	 * @return
 	 */
-	Double CalculateToDouble(IVariable<?>[] args);
+	Double CalculateToDouble(IVariable<?>[] vars);
 	/**
 	 * Takes in an array of Number based Variables and
 	 * processes them through this formula returning a
 	 * Float representation of the resulting answer.
-	 * @param args
+	 * @param vars	An array of Variable objects.
 	 * @return
 	 */
-	Float CalculateToFloat(IVariable<?>[] args);
+	Float CalculateToFloat(IVariable<?>[] vars);
 	/**
 	 * Takes in an array of Number based Variables and
 	 * processes them through this formula returning a
 	 * Long representation of the resulting answer.
-	 * @param args
+	 * @param vars	An array of Variable objects.
 	 * @return
 	 */
-	Long CalculateToLong(IVariable<?>[] args);
-	/**
-	 * Gets the string representation of the
-	 * derived formula.
-	 * @return
-	 */
-	String GetFormula();
-	/**
-	 * Gets the original config string that this formula
-	 * was derived from.
-	 * @return
-	 */
-	String GetOriginalFormula();
+	Long CalculateToLong(IVariable<?>[] vars);
 	/**
 	 * Gets the array size expected when passing arguments
 	 * to be processed by this formula.
-	 * @return
+	 * @return The input array size.
 	 */
-	int GetArgumentsLength();
+	Integer GetFormulaInputArraySize();
 	/**
-	 * Gets the human readable name for this formula.
-	 * @return
+	 * Gets the variable names used in the Formula Expression object.
+	 * @return An ArrayList<String> of variable names.
 	 */
+	ArrayList<String> GetFormulaExpressionVariableNames();
 	/**
 	 * Gets name of this Formula
-	 * @return FormulaName
+	 * @return The formula name (String).
 	 * @since 1.0
 	 */
 	String GetFormulaName();
 	/**
-	 * Gets name of this Formula
-	 * @return FormulaDesc
+	 * Gets the name of this Formula
+	 * @return The formula description (String).
 	 * @since 1.0
 	 */
-	String GetFromulaDesc();
+	String GetFormulaDesc();
 	/**
-	 * Gets name of this Formula
-	 * @return FormulaID
+	 * Gets the description of this Formula
+	 * @return The formula ID (String).
 	 * @since 1.0
 	 */
 	String GetFormulaId();
 	/**
-	 * Gets name of this Formula
-	 * @return FormulaEquation
+	 * Gets the equation string for this formula.
+	 * @return The formula equation (String).
 	 * @since 1.0
 	 */
 	String GetFormulaEquation();
-	/**
-	 * Gets name of this Formula
-	 * @return FormulaInputArray
-	 * @since 1.0
-	 */
-	Double GetFormulaInputArray();
-	/**
-	 * Sets the name of this formula
-	 * @param Name Formula Name of this variable
-	 * @since 1.0
-	 */
-	void SetFormulaName(String formulaName);
-	/**
-	 * Sets the description of this formula
-	 * @param Formula Description description
-	 * @since 1.0
-	 */
-	void SetFormulaDescription(String formulaDesc);
-	/**
-	 * Sets the id of this formula
-	 * @param Id of this formula id
-	 * @since 1.0
-	 */
-	void SetFormulaId(String formulaId);
-	/**
-	 * Sets the equation of this formula
-	 * @param equation formula equation
-	 * @since 1.0
-	 */
-	void SetFormulaEquation(String formulaEquation);
-	/**
-	 * Sets the value of this formula double
-	 * @param value Value of this formula
-	 * @since 1.0
-	 */
-	void SetFormulaInputArray(Double value);
-}
