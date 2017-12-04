e22b609bc79db695f8ecd1698da4fbae22c98147
Christopher Howard <choward13@mail.wou.edu>
2017-12-04 01:48:37 -0800

    Updated Formuli.java

--- src/com/roguedevstudios/uarg/System/Core/Elements/Formuli.java
+++ src/com/roguedevstudios/uarg/System/Core/Elements/Formuli.java
@@ -15,8 +15,10 @@ package com.roguedevstudios.uarg.System.Core.Elements;
 
 // Import Section
 import java.util.TreeMap;
+import java.util.ArrayList;
 import java.util.List;
 import com.roguedevstudios.uarg.System.Core.Elements.Interface.IFormula;
+import com.roguedevstudios.uarg.System.Core.Elements.Interface.IFormuli;
 
 /**
  * <p>
@@ -24,7 +26,7 @@ import com.roguedevstudios.uarg.System.Core.Elements.Interface.IFormula;
  * @author Terry Roberson
  * @since 1.0
  */
-public class Formuli {
+public class Formuli implements IFormuli {
 	private TreeMap<String, IFormula> _formulaMap;
 	private TreeMap<String, List<String>> _formulaSetMap;
 
@@ -40,8 +42,9 @@ public class Formuli {
 	 * Constructs the formuli container with formula variables and formula map
 	 * @since 1.0
 	 */
-	public Formuli(TreeMap<String, IFormula> _formulaMap, TreeMap<String, List<String>> _formulaSetMap) {
-		
+	public Formuli(TreeMap<String, IFormula> formulaMap, TreeMap<String, List<String>> formulaSetMap) {
+		this._formulaMap = formulaMap;
+		this._formulaSetMap = formulaSetMap;
 	}
 	
 	
@@ -52,6 +55,42 @@ public class Formuli {
 	private void _build() {
 		this._formulaMap = new TreeMap<String, IFormula>();
 		this._formulaSetMap = new TreeMap<String, List<String>>();
+	}
+
+	@Override
+	public List<String> GetFormulaSet(String ID) {
+		return this._formulaSetMap.get(ID);
+	}
+
+	@Override
+	public void SetFormulaSet(String ID) {
+		if(this._formulaSetMap.containsKey(ID))
+			return;
+		this._formulaSetMap.put(ID, new ArrayList<String>());
+		
+	}
+
+	@Override
+	public IFormula GetFormula(String ID) {
+		return this._formulaMap.get(ID);
+	}
+
+	@Override
+	public void AddFormulaToSet(String SetID, String FormulaID) {
+		if(!this._formulaSetMap.containsKey(SetID))
+			this.SetFormulaSet(SetID);
+		if(this._formulaSetMap.get(SetID).contains(FormulaID))
+			return;
+		this._formulaSetMap.get(SetID).add(FormulaID);
+		
+	}
+
+	@Override
+	public void AddFormula(String ID, IFormula formula) {
+		if(this._formulaMap.containsKey(ID))
+			return;
+		this._formulaMap.put(ID, formula);
+		
 	}
 	
 	
