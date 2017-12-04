90ae2c1ccf398ed11be6cd25230a9c239590b423
Christopher Howard <choward13@mail.wou.edu>
2017-12-04 00:34:04 -0800

    Matrix calculation adaptation & minor testing

--- src/com/roguedevstudios/uarg/System/Core/Tests/FormulaTest.java
+++ src/com/roguedevstudios/uarg/System/Core/Tests/FormulaTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import com.roguedevstudios.uarg.System.Core.Elements.Formula;
 import com.roguedevstudios.uarg.System.Core.Elements.Variable;
+import com.roguedevstudios.uarg.System.Core.Elements.Interface.IVariable;
 
 import java.util.ArrayList;
 
@@ -80,6 +81,113 @@ public class FormulaTest {
 		Integer out = formula.CalculateToInteger(v);
 		//System.out.println(out);
 		assertTrue(out==50);
+	}
+	
+	@Test
+	public void testFormulaProcessingLog() {
+		String formulaExpression = "log(_var_)";
+		Formula formula = new Formula("T","T","T",formulaExpression);
+		Variable<Integer> v1 = new Variable<Integer>("T","T",false,"T",4);
+		Variable<? extends Number>[] v = (Variable<? extends Number>[]) new Variable<?>[1];
+		v[0] = v1;
+		Double out = formula.CalculateToDouble(v);
+		Double outCheck = Math.log(4);
+		System.err.println("Exp4j Output: "+out);
+		System.err.println("Java Output: "+outCheck);
+		assertEquals(out,outCheck);
+	}
+	
+	@Test
+	public void testFormulaCrazy() {
+		String formulaExpression = "_var1_ + _var2_ + 1000 * _var3_ - ( _var4_ + _var5_ + _var6_ * _var8_ ) + _var9_ + _var7_";
+		Formula formula = new Formula("T","T","T",formulaExpression);
+		Variable<Integer> v1 = new Variable<Integer>("T","T",false,"T",4);
+		Variable<Double> v3 = new Variable<Double>("T","T",false,"T",5d);
+		Variable<Float> v2 = new Variable<Float>("T","T",false,"T",4.0f);
+		Variable<Long> v4 = new Variable<Long>("T","T",false,"T",25L);
+		Variable<Integer> v5 = new Variable<Integer>("T","T",false,"T",40);
+		Variable<Integer> v6 = new Variable<Integer>("T","T",false,"T",9);
+		Variable<Integer> v7 = new Variable<Integer>("T","T",false,"T",7);
+		Variable<Integer> v8 = new Variable<Integer>("T","T",false,"T",32);
+		Variable<Integer> v9 = new Variable<Integer>("T","T",false,"T",14);
+		Variable<? extends Number>[] v = (Variable<? extends Number>[]) new Variable<?>[9];
+		
+		v[0] = v1;
+		v[1] = v2;
+		v[2] = v3;
+		v[3] = v4;
+		v[4] = v5;
+		v[5] = v6;
+		v[6] = v7;
+		v[7] = v8;
+		v[8] = v9;
+		
+		Double[] ins = new Double[9];
+		
+		ins[0] = v1.GetValue().doubleValue();
+		ins[1] = v2.GetValue().doubleValue();
+		ins[2] = v3.GetValue().doubleValue();
+		ins[3] = v4.GetValue().doubleValue();
+		ins[4] = v5.GetValue().doubleValue();
+		ins[5] = v6.GetValue().doubleValue();
+		ins[6] = v7.GetValue().doubleValue();
+		ins[7] = v8.GetValue().doubleValue();
+		ins[8] = v9.GetValue().doubleValue();
+		
+		Double outCheck = 
+				ins[0] +
+				ins[1] +
+				(
+						1000d *
+						ins[2]
+						//5000
+				)
+				//5000+4+4=5008
+				-
+				(
+						ins[3] +
+						ins[4] +
+						(
+								ins[5] *
+								ins[7]
+										//9*7=63
+						)
+						//63+25+40=128
+				)
+				//5008-128=4880
+				+
+				ins[8] +
+				ins[6];
+				//4880+32+14=4926
+				// Java says 4626
+				// Java PEMDAS failure....
+		outCheck = 4926d;
+				
+		Double out = formula.CalculateToDouble(v);
+		System.err.println(out);
+		System.out.println(outCheck);
+		assertEquals(out,outCheck);
+	}
+	
+	@Test
+	public void testMatrixProcessing() {
+		Integer[] A = new Integer[] {2,3,4};
+		Integer[] B = new Integer[] {5,6,7};
+		Integer C = new Integer(10);
+		String E = "_A_ * _B_ * _C_";
+		Formula F = new Formula("T","T","T",E);
+		Variable<Integer[]> vA = new Variable<>("T","T",false,"T",A);
+		Variable<Integer[]> vB = new Variable<>("T","T",false,"T",B);
+		Variable<Integer> vC = new Variable<>("T","T",false,"T",C);
+		IVariable<?>[] v = new Variable<?>[] {vA,vB,vC};
+		Double[] d = F.CalculateToDouble(v, true);
+		for(Double di: d) {
+			System.err.println("Output: "+di);
+		}
+		assertTrue(d.length == 3);
+		assertTrue(d[0] == 100);
+		assertTrue(d[1] == 180);
+		assertTrue(d[2] == 280);
 	}
 	
 	/**
